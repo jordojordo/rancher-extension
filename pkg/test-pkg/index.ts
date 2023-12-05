fe3b661102bbe9effa88ed364191ed4aa4d8cabe
Jordon Leach <jordonleach@gmail.com>
2023-12-05 08:21:28 -0500

    Test breaking build

--- pkg/test-pkg/index.ts
+++ pkg/test-pkg/index.ts
@@ -10,5 +10,5 @@ export default function(plugin: IPlugin) {
   plugin.metadata = require('./package.json');
 
   // Load a product
-  plugin.addProduct(require('./product'));
+  plugin.addProduct(require('./productz')); // test break build
 }
